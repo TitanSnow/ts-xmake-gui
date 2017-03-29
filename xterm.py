9c029e52405bada552b2d1fdba977ee62c129ca8
TitanSnow <sweeto@live.cn>
2017-03-29 11:56:02 +0800

    echo Succeed or Fail

--- xterm.py
+++ xterm.py
@@ -2,7 +2,7 @@ import subprocess as sp
 def run(cmd):
     sp.Popen(["xterm","-e",cmd])
 def run_keep_window(cmd):
-    return run(cmd+";cat")
+    sp.Popen(["xterm","-e",cmd+";if test $? -eq 0;then echo 'Succeed';else echo 'Fail';fi;cat"])
 try:
     run("echo 'Hello xmake!'|cat")
 except:
