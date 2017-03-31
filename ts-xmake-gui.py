b92b53420aff1dff8f51da7a41857f863ed153e3
TitanSnow <sweeto@live.cn>
2017-03-31 11:30:13 +0800

    improve geometry

--- ts-xmake-gui.py
+++ ts-xmake-gui.py
@@ -16,51 +16,51 @@ class MainWin(tk.Frame):
 
     def createWidgets(self):
         self.label_project=tk.Label(self,text="Project")
-        self.label_project.pack()
+        self.label_project.grid(sticky=tk.W+tk.E+tk.N+tk.S,row=0,column=0,columnspan=6)
 
         self.projectdir_input_content=tk.StringVar()
         self.projectdir_input_content.set(os.getenv("HOME"))
         self.projectdir_input=tk.Entry(self,textvariable=self.projectdir_input_content)
-        self.projectdir_input.pack()
+        self.projectdir_input.grid(sticky=tk.W+tk.E+tk.N+tk.S,row=1,column=0,columnspan=4)
 
         self.browse_projectdir=tk.Button(self,text="Browse Project Dir",command=self.action_browse_projectdir)
-        self.browse_projectdir.pack()
+        self.browse_projectdir.grid(sticky=tk.W+tk.E+tk.N+tk.S,row=1,column=4,columnspan=2)
 
         self.label_action=tk.Label(self,text="Action")
-        self.label_action.pack()
+        self.label_action.grid(sticky=tk.W+tk.E+tk.N+tk.S,row=2,column=0,columnspan=6)
 
-        self.config=tk.Button(self,text="Config",command=self.action_config)
-        self.config.pack()
+        self.config=tk.Button(self,text="Config",command=self.action_config,width=10)
+        self.config.grid(sticky=tk.W+tk.E+tk.N+tk.S,row=3,column=0)
 
-        self.build=tk.Button(self,text="Build",command=self.action_build)
-        self.build.pack()
+        self.build=tk.Button(self,text="Build",command=self.action_build,width=20)
+        self.build.grid(sticky=tk.W+tk.E+tk.N+tk.S,row=3,column=1,columnspan=2)
 
-        self.rebuild=tk.Button(self,text="Rebuild",command=self.action_rebuild)
-        self.rebuild.pack()
+        self.rebuild=tk.Button(self,text="Rebuild",command=self.action_rebuild,width=10)
+        self.rebuild.grid(sticky=tk.W+tk.E+tk.N+tk.S,row=3,column=3)
 
-        self.clean=tk.Button(self,text="Clean",command=self.action_clean)
-        self.clean.pack()
+        self.clean=tk.Button(self,text="Clean",command=self.action_clean,width=10)
+        self.clean.grid(sticky=tk.W+tk.E+tk.N+tk.S,row=3,column=4)
 
-        self.distclean=tk.Button(self,text="Distclean",command=self.action_distclean)
-        self.distclean.pack()
+        self.distclean=tk.Button(self,text="Distclean",command=self.action_distclean,width=10)
+        self.distclean.grid(sticky=tk.W+tk.E+tk.N+tk.S,row=3,column=5)
 
         self.label_target=tk.Label(self,text="Target")
-        self.label_target.pack()
+        self.label_target.grid(sticky=tk.W+tk.E+tk.N+tk.S,row=4,column=0,columnspan=2)
 
-        self.target_list=tk.Listbox(self)
-        self.target_list.pack()
+        self.target_list=tk.Listbox(self,width=0)
+        self.target_list.grid(sticky=tk.W+tk.E+tk.N+tk.S,row=5,column=0,columnspan=2,rowspan=2)
 
         self.label_config=tk.Label(self,text="Config")
-        self.label_config.pack()
+        self.label_config.grid(sticky=tk.W+tk.E+tk.N+tk.S,row=4,column=2,columnspan=4)
 
         self.reload_conf=tk.Button(self,text="Load",command=self.action_reload_conf)
-        self.reload_conf.pack()
+        self.reload_conf.grid(sticky=tk.W+tk.E+tk.N+tk.S,row=5,column=2,columnspan=2)
 
         self.reconfig=tk.Button(self,text="Config",command=self.action_config)
-        self.reconfig.pack()
+        self.reconfig.grid(sticky=tk.W+tk.E+tk.N+tk.S,row=5,column=4,columnspan=2)
 
-        self.configarea=tk.Text(self)
-        self.configarea.pack()
+        self.configarea=tk.Text(self,width=0)
+        self.configarea.grid(sticky=tk.W+tk.E+tk.N+tk.S,row=6,column=2,columnspan=4)
 
         self.reflesh_target_list()
         self.reflesh_configarea()
