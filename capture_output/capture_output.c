0baee9250e78c88cc5191027df7264a448b13f12
TitanSnow <sweeto@live.cn>
2017-04-17 13:48:57 +0800

    use ansi c

--- capture_output/capture_output.c
+++ capture_output/capture_output.c
@@ -5,17 +5,20 @@ WINPTY_API LPCWSTR capture_output(
     LPCWSTR cwd /*OPTIONAL*/,
     LPCWSTR env /*OPTIONAL*/
 ){
-    winpty_config_t *config=winpty_config_new(0,0);
-    if(!config) return 0;
-    winpty_t *pty=winpty_open(config,0);
+    winpty_config_t *config;
+    winpty_t *pty;
+    winpty_spawn_config_t *spawn_config;
+    BOOL suc;
+    if(!(config=winpty_config_new(0,0))) return 0;
+    pty=winpty_open(config,0);
     winpty_config_free(config);
     if(!pty) return 0;
-    winpty_spawn_config_t *spawn_config=winpty_spawn_config_new(WINPTY_SPAWN_FLAG_AUTO_SHUTDOWN,appname,cmdline,cwd,env,0);
+    spawn_config=winpty_spawn_config_new(WINPTY_SPAWN_FLAG_AUTO_SHUTDOWN,appname,cmdline,cwd,env,0);
     if(!spawn_config){
         winpty_free(pty);
         return 0;
     }
-    BOOL suc=winpty_spawn(pty,spawn_config,0,0,0,0);
+    suc=winpty_spawn(pty,spawn_config,0,0,0,0);
     winpty_spawn_config_free(spawn_config);
     if(!suc){
         winpty_free(pty);
